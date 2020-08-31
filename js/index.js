563dc660b7852dc582e78b4b62153c2fcc9d31dd
YongHero <738255737@qq.com>
2020-08-31 12:45:49 +0800

    尝试grid布局

--- js/index.js
+++ js/index.js
@@ -1,72 +1,36 @@
-let timer = null;
-let container = document.getElementsByClassName('container')[0];
-let center = document.getElementsByClassName('center')[0];
-let colorArr = ['#CC9999', '#FFFFCC', '#CCCC99', '#FFCCCC', '#FFFF99', '#CCCCFF'];
-let num = 0; // 起始数字
-/**
- * 游戏开始
- */
-function start() {
-    timer = setInterval(() => {
-        let span = document.createElement('span');
-        span.innerHTML = num;
-        let bool = isRight(num);
-        let color = getRandom(colorArr.length, 0);
-        if (bool) {
-            span.style.color = colorArr[color];
-            span.style.textShadow = `0px 0px 3px ${colorArr[color]}`;
-            center.innerHTML = num;
-            var div = document.createElement('div');
-            div.innerHTML = num;
-            div.className = 'center';
-            div.style.color = colorArr[color];
-            setTimeout(() => {
-                div.style.transform = `translate(${getRandom(300, -300)}%,${getRandom(300, -300)}%)`;
-                div.style.opacity = '0';
-            }, 50)
-            document.body.appendChild(div);
-            // 动画结束移除dom
-            div.addEventListener('transitionend', () => {
-                div.remove();
-            })
-        }
-        container.appendChild(span);
-        //让滚动条一直滚动到最底部
-        // document.documentElement.scrollTop = document.documentElement.scrollHeight;
-        num++;
-        span.scrollIntoView();// 让页面滚动到该元素的位置
+let container = document.getElementById('divcontainer');
+let curDiv; // 保留拖拽的课程 用于放入课程表中
 
-    }, 100)
+container.ondragstart = (e) => {
+    curDiv = e.target;
+
 }
-function stop() {
-    clearInterval(timer);
-    timer = null;
-}
-/**
- * 判断是否满足条件，赋予颜色
- * @param {*} num 
- */
-function isRight(num) {
-    if (num < 2) {
-        return false;
+container.ondrop = (e) => {
+    let dom = e.target;
+
+    // 1.课程复制到课程表中
+    console.log( !!dom.dataset.nomove );
+    
+    if (curDiv.parentNode.tagName === 'DIV' && dom.tagName === 'LI' && !dom.dataset.nomove) {
+        let newDiv = curDiv.cloneNode(true);
+        dom.appendChild(newDiv)
     }
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) return false
+    // 2.课程表中的课程移动到其它位置
+    else if (e.target.parentNode.tagName === 'UL' && !dom.dataset.nomove) {
+        dom.appendChild(curDiv);
     }
-    return true
+    else if (e.target.parentNode.tagName === 'DIV' && curDiv.parentNode.tagName === 'LI') {
+        // 3.课程表中的课程移出课程表算是删除
+        curDiv.remove();
+    }
+
 }
-/**
- * 获取随机数
- * @param {*} max 
- * @param {*} min 
- */
-function getRandom(max, min) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-document.documentElement.onclick = function () {
-    if (timer) {
-        stop();
-    } else {
-        start();
+container.ondragover = (e) => {
+    e.preventDefault();
+    if( curDiv.parentNode.tagName === 'div') {
+        e.dataTransfer.dropEffect = 'copy'
+    }
+    else if (curDiv.parentNode.tagName === 'LI') {
+        e.dataTransfer.dropEffect = 'move';
     }
 }
